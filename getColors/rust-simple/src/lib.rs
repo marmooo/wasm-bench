1a71a4efa20c74a880c49240414a5ac24b617d28
marmooo <marmooo@users.noreply.github.com>
2024-10-05 20:25:05 +0900

    add rust-simple bench

---
+++ getColors/rust-simple/src/lib.rs
@@ -0,0 +1,65 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct ColorStat {
+    r: u8,
+    g: u8,
+    b: u8,
+    total: u32,
+}
+
+#[wasm_bindgen]
+impl ColorStat {
+    #[wasm_bindgen(constructor)]
+    pub fn new(r: u8, g: u8, b: u8, total: u32) -> ColorStat {
+        ColorStat { r, g, b, total }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn r(&self) -> u8 {
+        self.r
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn g(&self) -> u8 {
+        self.g
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn b(&self) -> u8 {
+        self.b
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn total(&self) -> u32 {
+        self.total
+    }
+}
+
+pub fn count_colors(uint8_data: &[u8]) -> Vec<u32> {
+    let uint32_data = unsafe {
+        std::slice::from_raw_parts(uint8_data.as_ptr() as *const u32, uint8_data.len() / 4)
+    };
+    let mut color_count = vec![0u32; 16777216];
+    for &rgba in uint32_data {
+        let rgb = rgba & 0xFFFFFF;
+        color_count[rgb as usize] += 1;
+    }
+    color_count
+}
+
+#[wasm_bindgen]
+pub fn get_colors(uint8_data: &[u8]) -> Vec<ColorStat> {
+    let color_count = count_colors(uint8_data);
+    let mut colors = Vec::new();
+    for (rgb, &uses) in color_count.iter().enumerate() {
+        if uses > 0 {
+            let r = (rgb & 0xFF) as u8;
+            let g = ((rgb >> 8) & 0xFF) as u8;
+            let b = ((rgb >> 16) & 0xFF) as u8;
+            let color = ColorStat::new(r, g, b, uses);
+            colors.push(color);
+        }
+    }
+    colors
+}
