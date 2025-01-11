6a93bbf20600b362fd7c8ee2ffb42b2b538e9c13
marmooo <marmooo@users.noreply.github.com>
2025-01-11 15:39:48 +0900

    fix ownership

--- countColors/rust-uint32/src/lib.rs
+++ countColors/rust-uint32/src/lib.rs
@@ -11,5 +11,5 @@ pub fn count_colors(uint8_data: &[u8]) -> Uint32Array {
         let rgb = rgba & 0xFFFFFF;
         color_count[rgb as usize] += 1;
     }
-    unsafe { Uint32Array::view(&color_count) }
+    Uint32Array::from(&color_count[..])
 }
